0295174562898af56edd4155bf1197b9ee50a3c2
vaishnav <147924923+vaishnav6@users.noreply.github.com>
2023-12-09 22:32:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,69 @@
-# Import necessary modules
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.camera import Camera
-from kivy.uix.image import Image
+from kivy.lang import Builder
 
-class ImageCaptureApp(App):
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+from kivymd.uix.responsivelayout import MDResponsiveLayout
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.imagelist import *
+
+KV = '''
+<MobileView>
+    BoxLayout:
+        orientation: 'vertical'
+
+        AsyncImage:
+            source: 'images\lion.png' 
+            size_hint_y: 0.7 
+        
+<TabletView>
+    BoxLayout:
+        orientation: 'vertical'
+
+        AsyncImage:
+            source: 'images\lion.png' 
+            size_hint_y: 0.7
+            
+<DesktopView>
+    BoxLayout:
+        orientation: 'vertical'
+
+        AsyncImage:
+            source: 'images\lion.png' 
+            size_hint_y: 0.7
+
+ResponsiveView:
+'''
+
+
+class CommonComponentLabel(MDLabel):
+    pass
+
+
+class MobileView(MDScreen):
+    pass
+
+
+class TabletView(MDScreen):
+    pass
+
+
+class DesktopView(MDScreen):
+    pass
+
+
+class ResponsiveView(MDResponsiveLayout, MDScreen):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        self.mobile_view = MobileView()
+        self.tablet_view = TabletView()
+        self.desktop_view = DesktopView()
+
+
+class Test(MDApp):
     def build(self):
-        # Create a layout
-        layout = BoxLayout(orientation='vertical')
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Orange"
+        return Builder.load_string(KV)
 
-        # Create a Camera widget
-        self.camera = Camera(resolution=(640, 480), play=True)
-        layout.add_widget(self.camera)
 
-        # Create buttons for capturing and viewing images
-        capture_button = Button(text="Capture", size_hint_y=None, height=50)
-        capture_button.bind(on_press=self.capture_image)
-        layout.add_widget(capture_button)
-
-        view_button = Button(text="View Image", size_hint_y=None, height=50)
-        view_button.bind(on_press=self.view_image)
-        layout.add_widget(view_button)
-
-        # Create an Image widget for displaying captured images
-        self.image_widget = Image()
-        layout.add_widget(self.image_widget)
-
-        return layout
-
-    def capture_image(self, instance):
-        # Capture the current frame from the camera and save it to a file
-        filename = "captured_image.png"
-        self.camera.export_to_png(filename)
-        print(f"Image captured and saved as {filename}")
-
-    def view_image(self, instance):
-        # Display the captured image in the Image widget
-        filename = "captured_image.png"
-        self.image_widget.source = filename
-        print(f"Viewing captured image: {filename}")
-
-if __name__ == '__main__':
-    ImageCaptureApp().run()
+Test().run()
