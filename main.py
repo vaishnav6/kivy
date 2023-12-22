5c10e9cb45a135060859808c7b528e2d87b5f59e
vaishnav <147924923+vaishnav6@users.noreply.github.com>
2023-12-22 17:30:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.clock import Clock
 
 class Vimoweb(MDApp):
-    current_index = 0
-    text_to_type = "VIMOWEB"
     def build(self):
         global sm
         sm = ScreenManager()
@@ -14,18 +12,10 @@ class Vimoweb(MDApp):
         return sm
     
     def on_start(self):
-        Clock.schedule_interval(self.typing, 0.2)
-        Clock.schedule_once(self.change_screen, 4)
+        Clock.schedule_once(self.change_screen, 3)
 
     def change_screen(self, dt):
         sm.current = "HomeScreen"
-
-    def typing(self, *args):
-        if self.current_index < len(self.text_to_type):
-            current_text = sm.get_screen("IntroScreen").ids.typings.text
-            current_text += self.text_to_type[self.current_index]
-            sm.get_screen("IntroScreen").ids.typings.text = current_text
-            self.current_index += 1
 
 if __name__ == '__main__':
     Vimoweb().run()
