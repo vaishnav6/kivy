daf4b1d64f2a3d129d9a88e8043c5354e04a8882
vaishnav <147924923+vaishnav6@users.noreply.github.com>
2023-12-22 17:05:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,31 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
-from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.clock import Clock
-
-kv = """
-MDFloatLayout:
-    md_bg_color: "black"
-    Image:
-        source: 'lion.png'
-        size_hint: None, None
-        size: 900, 900
-        pos_hint: {"center_x": 0.5, "center_y": 0.5}
-    Label:
-        id: typing
-        text: ""
-        color: "gold"
-        font_size: 50
-        bold: True
-        pos_hint: {"center_x": 0.5,"center_y": 0.29}
-        
-"""
 
 class Vimoweb(MDApp):
     current_index = 0
     text_to_type = "VIMOWEB"
-
     def build(self):
-        return Builder.load_string(kv)
+        global sm
+        sm = ScreenManager()
+        sm.add_widget(Builder.load_file("intro.kv"))
+        sm.add_widget(Builder.load_file("home.kv"))
+        return sm
     
+    def on_start(self):
+        Clock.schedule_interval(self.typing, 0.2)
+        Clock.schedule_once(self.change_screen, 4)
+
+    def change_screen(self, dt):
+        sm.current = "HomeScreen"
+
     def typing(self, *args):
         if self.current_index < len(self.text_to_type):
-            current_index = self.root.ids.typing.text
-            current_index += self.text_to_type[self.current_index]
-            self.root.ids.typing.text = current_index
+            current_text = sm.get_screen("IntroScreen").ids.typings.text
+            current_text += self.text_to_type[self.current_index]
+            sm.get_screen("IntroScreen").ids.typings.text = current_text
             self.current_index += 1
-        else:
-            Clock.unschedule(self.typing)
-        
-    def on_start(self):
-        Clock.schedule_interval(self.typing, 0.1)
 
 if __name__ == '__main__':
     Vimoweb().run()
